a82ee5f16035c714b1bf902774ccf40473eef4ea
961028 <emilcarlholmsten@gmail.com>
2023-01-18 01:01:22 +0100

    Storage rework again

--- storage.js
+++ storage.js
@@ -1,53 +1,47 @@
 function set(key, value) {
+    // Sets a key-value pair in the browser's local storage
     browser.storage.local.set({ [key]: value });
 }
 
 function get(key) {
+    // Gets the value of a key from the browser's local storage
     return browser.storage.local.get(key)
 }
 
 function remove(key) {
+    // Removes a key-value pair from the browser's local storage
     browser.storage.local.remove(key)
 }
 
+
 function listGet(key) {
-    return get(key).then(list => {
-        if (list[key] === undefined) {
-            list[key] = [];
+    // Gets the list stored under key in the browser's local storage
+    return get(key).then(result => {
+        if (result[key] === undefined) {
+            return [];
+        } else {
+            return result[key];
         }
-        return list[key];
     });
 }
 
-function listInsert(key, value) {
+function listAdd(key, value) {
+    // Adds value to the list stored under key in the browser's local storage
     return listGet(key).then(list => {
-        list.push(value);
-        set(key, list);
+        if (list.indexOf(value) === -1) {
+            list.push(value);
+            set(key, list);
+        }
     });
 }
 
 function listRemove(key, value) {
+    // Removes value from the list stored under key in the browser's local storage
     return listGet(key).then(list => {
-        list.splice(list.indexOf(value), 1);
-        set(key, list);
+        let index = list.indexOf(value);
+        if (index !== -1) {
+            list.splice(index, 1);
+            set(key, list);
+        }
     });
 }
-
-function listFind(key, value) {
-    return listGet(key).then(list => {
-        return list.indexOf(value) !== -1;
-    });
-}
-
-function listUpdate(key, value, newValue) {
-    return listGet(key).then(list => {
-        list[list.indexOf(value)] = newValue;
-        set(key, list);
-    });
-}
-
-function listClear(key) {
-    return set(key, []);
-}
-
-export default { set, get, remove, listGet, listInsert, listRemove, listFind, listUpdate, listClear }
