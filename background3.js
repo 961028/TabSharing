49dec4b4532648ec4fb3a32d217360a32e64d70e
961028 <emilcarlholmsten@gmail.com>
2023-02-03 12:45:31 +0100

    testing updates

--- background3.js
+++ background3.js
@@ -1,5 +1,7 @@
-async function simplifyTab(tab) {
-    return { title: tab.title, url: tab.url };
+async function generateStorageTab(tab) {
+    let storageTab = { title: tab.title, url: tab.url };
+    storageTab.storageId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    return storageTab;
 }
 
 /**
@@ -8,23 +10,73 @@ async function simplifyTab(tab) {
 */
 async function saveSession(sessionName) {
 
-    let sessions = await browser.storage.local.get() || {};
+    let sessions = await browser.storage.local.get() || [];
     sessions.forEach(session => {
-        if(sessionName === session) return;
+        if (sessionName === session) return;
     });
 
     let tabs = await browser.tabs.query({ currentWindow: true });
-    let simpleTabs;
+    let storageTabs;
     tabs.forEach(tab => {
-        let simpleTab = simplifyTab(tab);
-        simpleTabs.push(simpleTab);
+        let storageTab = generateStorageTab(tab);
+        storageTabs.push(storageTab);
     });
 
-    await browser.storage.local.set(sessionName, simpleTabs);
+    await browser.storage.local.set(sessionName, storageTabs);
     
     let currentWindow = await browser.windows.getLastFocused();
     await browser.sessions.setWindowValue(currentWindow.id, 'currentSession', sessionName);
 
-    browser.runtime.sendMessage({type: 'displayTabs', tabs: simpleTabs});
+    browser.runtime.sendMessage({type: 'displayTabs', tabs: storageTabs});
     browser.runtime.sendMessage({type: 'displaySessions', sessions: sessions, highlightedSession: sessionName});
+}
+
+async function storageUpdate(changes) {
+    let changedItems = Object.keys(changes);
+
+    for (const key of changedItems) {
+        let currentValue = await browser.storage.local.get(key);
+        let newValue = changes[key].newValue;
+        if (currentValue !== newValue) {
+            await browser.storage.local.set(key, newValue);
+                    
+            const newTabs = newValue.filter(tab => {
+                return !(browser.storage.local.get(tab.storageId));
+            });
+            
+            if (newTabs.length > 0) {
+                newTabs.forEach(tab => {
+                    browser.tabs.create({
+                        title: tab.title,
+                        url: tab.url
+                    });
+                });
+            }
+        }
+    }
+}
+browser.storage.onChanged.addListener(testingStorageUpdate);
+
+function compareTabs(oldTabs, newTabs) {
+    const created = [];
+    const removed = [];
+    const updated = [];
+  
+    for (const newTab of newTabs) {
+      const oldTab = oldTabs.find(tab => tab.storageId === newTab.storageId);
+      if (!oldTab) {
+        created.push(newTab);
+      } else if (oldTab.url !== newTab.url) {
+        updated.push(newTab);
+      }
+    }
+  
+    for (const oldTab of oldTabs) {
+      const newTab = newTabs.find(tab => tab.storageId === oldTab.storageId);
+      if (!newTab) {
+        removed.push(oldTab);
+      }
+    }
+  
+    return { created, removed, updated };
   }
