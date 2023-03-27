8dc93886e6f0bb9e557b50922327eb0c0690a091
961028 <emilcarlholmsten@gmail.com>
2023-03-27 16:12:48 +0200

    rework

--- background.js
+++ background.js
@@ -1,256 +1,69 @@
-const currentSession = 'CurrentSession';
-const sessionsList = 'Sessions';
+const sessions = new Map();
 
-/**
-* Sets a key/value pair in the storage.
-* @param {string} key The key to set.
-* @param {any} value The value to set.
-*/
-function set(key, value) {
-  const data = { [key]: value };
-  browser.storage.local.set(data);
-  //local[key] = value;
+function generateTemporaryName() {
+  const timestamp = new Date().toISOString();
+  return `Session_${timestamp}`;
 }
 
-/**
-* Gets a value from the storage.
-* @param {string} key The key to get.
-* @returns {Promise<any>} The value.
-*/
-async function get(key) {
-  const result = await browser.storage.local.get(key);
-  return result[key];
-  //return local[key];
-}
-
-/**
-* Gets a list from the storage.
-* @param {string} key The key to get.
-* @returns {Promise<any[]>} The list.
-*/
-async function getList(key) {
-  const value = await get(key);
-  return value || [];
-}
-
-/**
-* Pushes a value to a list in the storage.
-* @param {string} key The key of the list.
-* @param {any} value The value to push.
-*/
-async function push(key, value) {
-  const list = await getList(key);
-  list.push(value);
-  set(key, list);
-}
-
-/**
-* Pushes multiple values to a list in the storage.
-* @param {string} key The key of the list.
-* @param {any} value The values to push.
-*/
-async function pushMultiple(key, values) {
-  const list = await getList(key);
-  values.forEach(value => {
-    list.push(value);
-  });
-  set(key, list);
-}
-
-/**
-* Removes a value from a list in the storage.
-* @param {string} key The key of the list.
-* @param {any} value The value to remove.
-*/
-async function removeFromList(key, value) {
-  const list = await getList(key);
-  const index = list.indexOf(value);
-  if (index > -1) {
-    list.splice(index, 1);
-    set(key, list);
-  }
-}
-
-/**
-* Sets the current window session.
-* @param {string} name The name of the session.
-*/
-async function setWindowSession(name) {
-  await set(currentSession, name);
-}
-
-/**
-* Gets the current window session.
-* @returns {Promise<string>} The name of the session.
-*/
-async function getCurrentSession() {
-  let session = await get(currentSession);
-  if (!session) {
-    setWindowSession('Unnamed Session');
-  }
+async function saveSession(sessionName, tabs) {
+  const session = { name: sessionName, tabs: tabs };
+  sessions.set(sessionName, session);
   return session;
 }
 
-/**
-* Saves the current window session.
-* @param {string} sessionName The name of the session.
-*/
-async function saveSession(sessionName) {
-  const sessions = await getList(sessionsList);
-  if(sessions.includes(sessionName)) return;
-  const tabs = await browser.tabs.query({ currentWindow: true });
-  await set(sessionName, tabs);
-  await setWindowSession(sessionName);
-  //await push(sessionsList, sessionName);
-  displayTabs();
-  displaySessions();
+async function getSession(sessionName) {
+  return sessions.get(sessionName);
 }
 
-/**
-* Opens a saved session.
-* @param {string} sessionName The name of the session.
-*/
-let isUpdating = false;
-async function openSession(sessionName) {
-  if (isUpdating) return;
-  isUpdating = true;
-  /*
-  const windowSession = await getCurrentSession();
-  if (!windowSession) {
-    const result = confirm (
-      'You are about to open a saved session. This will overwrite the current unsaved session. Do you want to proceed?'
-    );
-    if (!result) {
-      return;
+async function getSessions() {
+  return Array.from(sessions.values());
+}
+
+async function deleteSession(sessionName) {
+  return sessions.delete(sessionName);
+}
+
+function updateTabInSession(sessionName, tabId, updatedProperties) {
+  const session = sessions.get(sessionName);
+  if (session) {
+    const tabIndex = session.tabs.findIndex((tab) => tab.id === tabId);
+    if (tabIndex !== -1) {
+      session.tabs[tabIndex] = { ...session.tabs[tabIndex], ...updatedProperties };
     }
   }
-*/
-  const tabs = await browser.tabs.query({ currentWindow: true });
-  let newTabs = await getList(sessionName);
-  await openTabs(newTabs);
-  await closeTabs(tabs);
-
-  await setWindowSession(sessionName);
-  displayTabs();
-
-  isUpdating = false;
 }
 
-/**
-* Closes a list of tabs.
-* @param {browser.tabs.Tab[]} tabs The tabs to close.
-*/
-async function closeTabs(tabs) {
-  for (const tab of tabs) {
-    await browser.tabs.remove(tab.id);
+function removeTabFromSession(sessionName, tabId) {
+  const session = sessions.get(sessionName);
+  if (session) {
+    session.tabs = session.tabs.filter((tab) => tab.id !== tabId);
   }
 }
 
-/**
-* Opens a list of tabs.
-* @param {browser.tabs.Tab[]} tabs The tabs to open.
-*/
-async function openTabs(tabs) {
-  for (const tab of tabs) {
-    browser.tabs.create({
-        url: tab.url
-    });
-  }
-}
-
-/* ---- LISTENER FUNCTIONS ---- */
-
-/**
-* Updates a tab in the storage.
-* @param {number} tabId The id of the tab.
-*/
-async function updateStorageTab(tabId) {
-  if (isUpdating) return;
-  const sessionName = await getCurrentSession();
-  const [tabs, updatedTab] = await Promise.all([
-    getList(sessionName), 
-    browser.tabs.get(tabId)
-  ]);
-  const tabIndex = tabs.findIndex(tab => tab.id === tabId);
-  if (tabIndex !== -1) {
-    tabs[tabIndex] = updatedTab;
-  }
-  set(sessionName, tabs);
-  displayTabs();
-}
-
-/**
-* Creates a tab in the storage.
-* @param {browser.tabs.Tab} tab The tab to create.
-*/
-async function createStorageTab(tab) {
-  if (isUpdating) return;
-  const sessionName = await getCurrentSession();
-  push(sessionName, tab);
-  displayTabs();
-}
-
-/**
-* Removes a tab from the storage.
-* @param {number} tabId The id of the tab.
-*/
-async function removeStorageTab(tabId) {
-  if (isUpdating) return;
-  const sessionName = await getCurrentSession();
-  const tabs = await getList(sessionName);
-  const filteredTabs = tabs.filter(tab => tab.id !== tabId);
-  set(sessionName, filteredTabs);
-  displayTabs();
-}
-
-/* ---- LISTENERS AND MESSAGING ---- */
-
-browser.runtime.onMessage.addListener(data => {
-  switch (data.type) {
-    case 'saveSession':
-      saveSession(data.name);
-      break;
-    case 'openSession':
-      openSession(data.name);
-      break;
-    case 'refresh':
-      displayTabs();
-      displaySessions();
-      break;
+browser.runtime.onMessage.addListener((message) => {
+  if (message.type === 'saveSession') {
+    const { sessionName, tabs } = message;
+    return saveSession(sessionName, tabs);
+  } else if (message.type === 'getSession') {
+    return getSession(message.sessionName);
+  } else if (message.type === 'deleteSession') {
+    return deleteSession(message.sessionName);
+  } else if (message.type === 'getSessions') {
+    return getSessions();
   }
 });
 
-/**
-* Displays the tabs in the popup.
-*/
-async function displayTabs() {
-  const sessionName = await getCurrentSession();
-  if (!sessionName) return;
-  const tabs = await getList(sessionName);
-  browser.runtime.sendMessage({type: 'displayTabs', tabs: tabs});
-}
+browser.tabs.onCreated.addListener(async (tab) => {
+  const currentSession = await getSession("current");
+  currentSession.tabs.push(tab);
+});
 
-/**
-* Displays the sessions in the popup.
-*/
-async function displaySessions() {
-  const allSessionNames = await getList(sessionsList);
-  if(allSessionNames === []) return;
-  const currentSessionName = await getCurrentSession();
-  browser.runtime.sendMessage({type: 'displaySessions', sessions: allSessionNames, highlightedSession: currentSessionName});
-}
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  const currentSession = await getSession("current");
+  updateTabInSession(currentSession.name, tabId, changeInfo);
+});
 
-/**
-* Displays a message in the popup using alert.
-* @param {string} message The message to display.
-*/
-function popup(message) {
-  browser.runtime.sendMessage({type: 'popup', message: message});
-}
-
-browser.tabs.onUpdated.addListener(updateStorageTab);
-browser.tabs.onCreated.addListener(createStorageTab);
-browser.tabs.onRemoved.addListener(removeStorageTab);
-
-displayTabs();
-displaySessions();
+browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
+  const currentSession = await getSession("current");
+  removeTabFromSession(currentSession.name, tabId);
+});
--- popup.js
+++ popup.js
@@ -1,67 +1,43 @@
-const saveButton = document.getElementById('save-button');
-const openButton = document.getElementById('open-button');
-const refreshButton = document.getElementById('refresh-button');
-const nameInput = document.getElementById('name-input');
-const tabs = document.getElementById('tabs');
-const sessions = document.getElementById('sessions');
-
-async function displayTabs(listOfTabs) {
-  const tabList = document.createElement('ul');
-  listOfTabs.forEach(tab => {
-    const tabItem = document.createElement('li');
-    tabItem.innerText = tab.title;
-    tabList.appendChild(tabItem);
-    const tabId = document.createElement('li');
-    tabId.innerText = tab.id;
-    tabList.appendChild(tabId);
-  });
-  document.getElementById('tabs').innerHTML = '';
-  document.getElementById('tabs').appendChild(tabList);
-}
-
-async function displaySessions(listOfSessions, highlightedSession) {
-  const sessionList = document.createElement('ul');
-  listOfSessions.forEach(sessionName => {
-    const sessionItem = document.createElement('li');
-    if (sessionName == highlightedSession) {
-      sessionItem.innerText = 'current: ' + sessionName;
-    } else {
-      sessionItem.innerText = sessionName;
-    }
-    sessionList.appendChild(sessionItem);
-  });
-  document.getElementById('sessions').innerHTML = '';
-  document.getElementById('sessions').appendChild(sessionList);
-}
-
-browser.runtime.onMessage.addListener(data => {
-  switch (data.type) {
-    case 'displayTabs':
-      displayTabs(data.tabs);
-      break;
-    case 'displaySessions':
-      displaySessions(data.sessions, data.highlightedSession);
-      break;
-    case 'popup':
-      alert(data.message);
-      break;
+async function saveCurrentSession() {
+    const tabs = await browser.tabs.query({ currentWindow: true });
+    const sessionName = document.getElementById('sessionName').value || generateTemporaryName();
+    browser.runtime.sendMessage({ type: 'saveSession', sessionName, tabs });
+    document.getElementById('sessionName').value = '';
   }
-});
+  
+  async function restoreSession() {
+    const sessionName = document.getElementById('sessionsList').value;
+    const session = await browser.runtime.sendMessage({ type: 'getSession', sessionName });
+    if (session) {
+      const currentTabs = await browser.tabs.query({ currentWindow: true });
+      currentTabs.forEach((tab) => browser.tabs.remove(tab.id));
+  
+      session.tabs.forEach((tab) => browser.tabs.create({ url: tab.url }));
+    }
+  }
+  
+  async function deleteSelectedSession() {
+    const sessionName = document.getElementById('sessionsList').value;
+    await browser.runtime.sendMessage({ type: 'deleteSession', sessionName });
+  }
 
-function refresh() {
-  browser.runtime.sendMessage({type: 'refresh'});
-}
+  async function populateSessionsList() {
+    const sessionsList = document.getElementById('sessionsList');
+    sessionsList.innerHTML = '';
+  
+    const sessions = await browser.runtime.sendMessage({ type: 'getSessions' });
+    sessions.forEach((session) => {
+      const option = document.createElement('option');
+      option.value = session.name;
+      option.textContent = session.name;
+      sessionsList.appendChild(option);
+    });
+  }
 
-function saveSession() {
-  browser.runtime.sendMessage({type: 'saveSession', name: nameInput.value});
-}
-
-function openSession() {
-  browser.runtime.sendMessage({type: 'openSession', name: nameInput.value});
-}
-
-saveButton.addEventListener('click', saveSession);
-openButton.addEventListener('click', openSession);
-refreshButton.addEventListener('click', refresh);
-
-refresh();
+  populateSessionsList();
+  
+  document.getElementById('sessionsList').addEventListener('change', restoreSession);  
+  document.getElementById('saveBtn').addEventListener('click', saveCurrentSession);
+  document.getElementById('restoreBtn').addEventListener('click', restoreSession);
+  document.getElementById('deleteBtn').addEventListener('click', deleteSelectedSession);
+  
